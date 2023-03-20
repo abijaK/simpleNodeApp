15836d3226625861e75d4cd42eb3bb18d34d9c69
AbijaK <abijamk@gmail.com>
2023-03-20 12:20:32 +0100

    wait to fix bootstrap to run in server

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ const __dirname = path.resolve();
 const app = express();
 const port = 4500;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/assets"));
+
+app.use("/css", express.static(path.join(__dirname + "node_modules/bootstrap/dist/css")));
+
+app.use("/js", express.static(path.join(__dirname + "node_modules/bootstrap/dist/js")));
+
+app.use("/js", express.static(path.join(__dirname + "node_modules/jquery/js")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "pages","index.html"));
+    res.sendFile(path.join(__dirname, "pages", "index.html"));
 })
 
 app.listen(port, () => {
